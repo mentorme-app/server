ac9b8f4319e7a3276167f1e207836124f0e86f8b
Pav0l <pavol.karas@gmail.com>
2019-06-22 18:31:36 +0200

    Clean up entry index.js file

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const compression = require('compression');
-
-const app = express();
-
-app.use(express.json());
-app.use(compression());
-app.use(helmet());
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.status(200).json('Hello from server!');
-});
+const server = require('./api/index');
 
 const PORT = process.env.PORT || '9000';
 
-app.listen(PORT, () => {
-    console.log(`*** Server listening on port: ${PORT}***`);
+server.listen(PORT, () => {
+    console.log(
+        `*** Server listening on port: ${PORT} in ${
+            server.settings.env
+        } mode ***`
+    );
 });
