226225a3b8bc719c289ab3b1f1ce307ca563279c
Pav0l <pavol.karas@gmail.com>
2019-06-24 15:42:06 +0200

    Add returning ID for Postgres

--- models/users.js
+++ models/users.js
@@ -2,9 +2,15 @@ const db = require('../data/db');
 
 module.exports = {
     getAll: () => db('users'),
-    filter: query => db('users').where(query),
+    filter: query =>
+        db('users')
+            .where(query)
+            .returning('id'),
     getById: id => db('users').where({ id }),
-    addUser: user => db('users').insert(user),
+    addUser: user =>
+        db('users')
+            .insert(user)
+            .returning('id'),
     update: (user_id, changes) =>
         db('users')
             .where({ user_id })
