8fc39a2ff87b06c566582ec970e0ec5078722ff4
Pav0l <pavol.karas@gmail.com>
2019-06-24 15:34:03 +0200

    Finalize auth API endpoints

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,53 +2,81 @@ const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 const validate = require('../../middleware/validate');
 const generateToken = require('../../lib/generateToken');
+const User = require('../../models/users');
 
 /**
- * @todo - add validation object to middlewares
- * @todo - implement data models
+ * @todo - add validation middleware
  *
  */
-router.post('/login', validate(), async (req, res) => {
-    const { username, password } = req.body;
-    // fetch user from DB
-    const user = username; // TBD
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        // fetch user from DB
+        const [user] = await User.filter({ email });
 
-    if (user && bcrypt.compareSync(password, user.password)) {
-        delete user.password;
+        if (user && bcrypt.compareSync(password, user.password)) {
+            delete user.password;
 
-        /**
-         * generateToken takes in
-         * { id, username, role }
-         */
-        const token = generateToken(user);
+            /**
+             * generateToken takes in
+             * { id, email }
+             */
+            const token = generateToken(user);
 
-        res.status(200).json({
-            user,
-            token
-        });
-    } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+            res.status(200).json({
+                user,
+                token
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid Credentials' });
+        }
+    } catch (err) {
+        res.send(500).json({ error: err });
     }
 });
 
 // add validation object
-router.post('/register', validate(), async (req, res) => {
-    const { username, email, password } = req.body;
+router.post('/register', async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
 
-    // check if username/email already exist in database
+        // check if username/email already exist in database
+        const [user] = await User.filter({ email });
 
-    // encrypt password
+        if (user) {
+            return res
+                .status(400)
+                .json({ message: 'This email is already taken' });
+        }
 
-    // save user to db
+        // encrypt password
+        password = bcrypt.hashSync(password, 10);
 
-    // remove password from user response
+        // save user to db
+        const [newUser] = await User.addUser({ username, email, password });
 
-    // generate token
+        if (newUser) {
+            const [getNewUser] = await User.getById(newUser);
+            // remove password from user response
+            delete getNewUser.password;
 
-    // send token back in response together with required user info
-    // but without password!
+            // generate token
+            /**
+             * generateToken takes in
+             * { id, email }
+             */
+            const token = generateToken(getNewUser);
+            // send token back in response together with required user info
+            // but without password!
 
-    res.status(201).json('Created new user');
+            res.status(201).json({
+                user: getNewUser,
+                token
+            });
+        }
+    } catch (err) {
+        res.send(500).json({ error: err });
+    }
 });
 
 module.exports = router;
