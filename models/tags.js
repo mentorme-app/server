771d3f2a729cec186f0a3021162ea5dbea60b663
Pav0l <pavol.karas@gmail.com>
2019-06-25 11:16:56 +0200

    Fix tag id

--- models/tags.js
+++ models/tags.js
@@ -3,5 +3,5 @@ const db = require('../data/db');
 module.exports = {
     getAll: () => db('tags'),
     filter: query => db('tags').where(query),
-    getById: tag_id => db('tags').where({ tag_id })
+    getById: id => db('tags').where({ id })
 };
