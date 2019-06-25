96b16d94d06448207850eb68a2d7abd067d93fc4
Pav0l <pavol.karas@gmail.com>
2019-06-25 13:41:03 +0200

    Remove last migration

--- data/migrations/20190625130434_03-Add_default_tags.js
+++
@@ -1,6 +0,0 @@
-exports.up = knex =>
-    knex.schema.raw(
-        'ALTER TABLE "users" ALTER COLUMN "tag_id" SET DEFAULT "1"'
-    );
-
-exports.down = knex => knex.schema.dropTableIfExists('users');
