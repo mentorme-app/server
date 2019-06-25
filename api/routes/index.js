a9633ae4c96ebfff1c5b7a15dd5f7ae592adb01d
Pav0l <pavol.karas@gmail.com>
2019-06-25 14:10:38 +0200

    Wire up Questions endpoints

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,14 @@
-const { auth, user, tags } = require('../../lib/routes');
+const { auth, user, tags, questions } = require('../../lib/routes');
 const authRouter = require('./auth');
 const userRouter = require('./user');
 const tagsRouter = require('./tags');
+const questionsRouter = require('./questions');
 
 module.exports = server => {
     server.use(auth, authRouter);
     server.use(user, userRouter);
     server.use(tags, tagsRouter);
+    server.use(questions, questionsRouter);
 
     server.get('/', (req, res) => {
         res.status(200).json('MentorMe API is live!');
