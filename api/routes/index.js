533b1dc8ed0e7125e14eaa072dab023ac0e07a4c
Pav0l <pavol.karas@gmail.com>
2019-06-25 11:32:35 +0200

    Wire up Tags API

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,12 @@
-const { auth, user } = require('../../lib/routes');
+const { auth, user, tags } = require('../../lib/routes');
 const authRouter = require('./auth');
 const userRouter = require('./user');
+const tagsRouter = require('./tags');
 
 module.exports = server => {
     server.use(auth, authRouter);
     server.use(user, userRouter);
+    server.use(tags, tagsRouter);
 
     server.get('/', (req, res) => {
         res.status(200).json('MentorMe API is live!');
