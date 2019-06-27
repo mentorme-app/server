2a73f25f6629a05bb54e1069043f238d833c341a
Pav0l <pavol.karas@gmail.com>
2019-06-27 13:37:47 +0200

    Import paths

--- tests/users.test.js
+++ tests/users.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const bcrypt = require('bcryptjs');
 const server = require('../api/index');
 const db = require('../data/db');
+const path = require('../lib/routes');
 const { user, tag, putUser } = require('./testsSetup');
 
 describe('Users endpoints', function() {
@@ -13,16 +14,17 @@ describe('Users endpoints', function() {
 
     afterEach(async function() {
         await db('users').truncate();
+        await db('tags').truncate();
     });
 
     describe('GET /:id', function() {
         it('Returns status code 200 on succesfull user fetch', async function() {
-            const res = await request(server).get('/api/user/1');
+            const res = await request(server).get(`${path.user}/1`);
             assert.strictEqual(res.status, 200, 'Status codes 200 are equal');
         });
 
         it('Returns the user object if it exist', async function() {
-            const res = await request(server).get('/api/user/1');
+            const res = await request(server).get(`${path.user}/1`);
             assert.property(res.body, 'id', 'Contains user id');
             assert.strictEqual(res.body.id, 1, 'Responds with correct ID');
             assert.property(res.body, 'username', 'Contains username');
@@ -40,12 +42,12 @@ describe('Users endpoints', function() {
         });
 
         it('Sends 404 status code if a user with ID does not exist', async function() {
-            const res = await request(server).get('/api/user/5');
+            const res = await request(server).get(`${path.user}/5`);
             assert.strictEqual(res.status, 404, 'Status codes 404 are equal');
         });
 
         it('Sends an error message if user with ID does not exist', async function() {
-            const res = await request(server).get('/api/user/5');
+            const res = await request(server).get(`${path.user}/5`);
             assert.property(res.body, 'message', 'Contains error message');
         });
     });
