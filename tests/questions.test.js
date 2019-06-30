a26afaf1f03fdd29d7acbe79d94bde63c65bb4f2
Pav0l <pavol.karas@gmail.com>
2019-06-30 09:58:26 +0200

    POST and DEL question endpoints tests

--- tests/questions.test.js
+++ tests/questions.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const server = require('../api/index');
 const db = require('../data/db');
 const path = require('../lib/routes');
-const { user, tag, id, question } = require('./testsSetup');
+const { user, tag, id, question, postQ } = require('./testsSetup');
 
 describe('Questions endpoints', function() {
     before(async function() {
@@ -23,7 +23,6 @@ describe('Questions endpoints', function() {
             const res = await request(server).get(`${path.questions}`);
             assert.strictEqual(res.status, 200, 'Status codes 200 are equal');
         });
-
         it('Returns the questions array if it exist', async function() {
             const resArr = await request(server).get(`${path.questions}`);
             const res = resArr.body[0];
@@ -41,6 +40,12 @@ describe('Questions endpoints', function() {
                 question.author_id,
                 'ID in author object is equal to request author_id'
             );
+            assert.notProperty(res, 'password', 'Does not containt pw');
+            assert.notProperty(
+                res,
+                'phone_number',
+                'Does not containt phone number'
+            );
         });
     });
 
@@ -48,6 +53,29 @@ describe('Questions endpoints', function() {
         it('Returns status code 200 on success', async function() {
             const res = await request(server).get(`${path.questions}/${id}`);
             assert.strictEqual(res.status, 200, 'Status codes 200 are equal');
+        });
+        it('Returns the question object', async function() {
+            const res = await request(server).get(`${path.questions}/${id}`);
+            assert.property(res.body, 'title', 'Contains title prop');
+            assert.strictEqual(
+                res.body.title,
+                question.title,
+                'Responds with title'
+            );
+            assert.property(res.body, 'author', 'Contains author prop');
+            assert.isObject(res.body.author, 'Author prop contains object');
+            assert.property(res.body.author, 'id', 'Author object contains id');
+            assert.strictEqual(
+                res.body.author.id,
+                question.author_id,
+                'ID in author object is equal to request author_id'
+            );
+            assert.notProperty(res.body, 'password', 'Does not containt pw');
+            assert.notProperty(
+                res.body,
+                'phone_number',
+                'Does not containt phone number'
+            );
         });
         it('Sends 404 status code if a user with ID does not exist', async function() {
             const res = await request(server).get(`${path.questions}/5`);
@@ -57,6 +85,67 @@ describe('Questions endpoints', function() {
         it('Sends an error message if user with ID does not exist', async function() {
             const res = await request(server).get(`${path.questions}/5`);
             assert.property(res.body, 'message', 'Contains error message');
+        });
+    });
+
+    describe('POST question', function() {
+        it('Sends 404 if author_id does not exist', async function() {
+            const badQ = { ...postQ, author_id: 5 };
+            const res = await request(server)
+                .post(`${path.questions}`)
+                .send(badQ);
+            assert.strictEqual(res.status, 404, 'Status coded 404 match');
+        });
+        it('Sends 404 if tag_id does not exist', async function() {
+            const badQ = { ...postQ, tag_id: 5 };
+            const res = await request(server)
+                .post(`${path.questions}`)
+                .send(badQ);
+            assert.strictEqual(res.status, 404, 'Status coded 404 match');
+        });
+        it('Sends 422 on failed data validation', async function() {
+            const badQ = { ...postQ, id: 5 };
+            const res = await request(server)
+                .post(`${path.questions}`)
+                .send(badQ);
+            assert.strictEqual(res.status, 422, 'Status coded 422 match');
+        });
+        it('Returns status code 201 on success', async function() {
+            const res = await request(server)
+                .post(`${path.questions}`)
+                .send(postQ);
+            assert.strictEqual(res.status, 201, 'Status codes 201 are equal');
+        });
+        it('Returns newly created question object', async function() {
+            const res = await request(server)
+                .post(`${path.questions}`)
+                .send(postQ);
+            assert.property(res.body, 'id', 'Does have an ID');
+            assert.property(res.body, 'author', 'Does have author prop');
+            assert.property(res.body, 'tag', 'Does have tag prop');
+            assert.notProperty(
+                res.body,
+                'author_id',
+                'Does not have author_id prop'
+            );
+            assert.notProperty(res.body, 'tag_id', 'Does not have tag_id prop');
+        });
+    });
+    describe('DELETE question by ID', function() {
+        it('Sends 404 status code if a question with ID does not exist', async function() {
+            const res = await request(server).delete(`${path.questions}/5`);
+            assert.strictEqual(res.status, 404, 'Status codes 404 are equal');
+        });
+        it('Sends an error message if question with ID does not exist', async function() {
+            const res = await request(server).delete(`${path.questions}/5`);
+            assert.property(res.body, 'message', 'Contains error message');
+        });
+        it('Deletes the correct resource', async function() {
+            const [before] = await db('questions').where({ id });
+            await request(server).delete(`${path.questions}/${id}`);
+            const [after] = await db('questions').where({ id });
+            assert.exists(before, 'Resource with ID exists');
+            assert.notExists(after, 'The resource does no longer exist');
         });
     });
 });
