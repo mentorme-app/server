678376cbe9e92214f8c8f5b61a069aee08ccd9f9
Pav0l <pavol.karas@gmail.com>
2019-07-01 07:50:31 +0200

    Start IDs at 1000

--- tests/testsSetup.js
+++ tests/testsSetup.js
@@ -1,6 +1,9 @@
+const id = 1000;
+const badId = 5000;
+
 module.exports = {
     user: {
-        id: 1,
+        id: id,
         username: 'Joe',
         email: 'joe@gmail.com',
         password: 'secret',
@@ -8,32 +11,34 @@ module.exports = {
         avatar: null,
         motto: 'Hi! I am Joe tester',
         description: 'You can meet me in Testing enviroments testing software',
-        tag_id: 1
+        tag_id: id
     },
     tag: {
-        id: 1,
+        id: id,
         tag: 'Testing'
     },
     putUser: {
         username: 'Joey',
         password: 'not_so_secret'
     },
-    id: 1,
+    id: id,
+    badId: badId,
     question: {
+        id: id,
         title: 'How do you test API endpoints?',
         question:
             'What do you test in them? How do you test latency? How do you do penetration tests?',
-        author_id: 1,
-        tag_id: 1
+        author_id: id,
+        tag_id: id
     },
     postQ: {
         title: 'How do you POST new question?',
         question: 'What do you test in them?',
-        author_id: 1,
-        tag_id: 1
+        author_id: id,
+        tag_id: id
     },
     mentor: {
-        id: 2,
+        id: id + 1,
         username: 'Mentor',
         email: 'mentor@gmail.com',
         password: 'secret',
@@ -41,11 +46,11 @@ module.exports = {
         avatar: null,
         motto: 'Hi! I am Mentor tester',
         description: 'You can meet me in Testing enviroments testing software',
-        tag_id: 1
+        tag_id: id
     },
     conversation: {
-        id: 1,
-        question_id: 1,
-        mentor_id: 2
+        id: id,
+        question_id: id,
+        mentor_id: id + 1
     }
 };
